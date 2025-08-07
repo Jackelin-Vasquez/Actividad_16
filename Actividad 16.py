49abc1e39c0ddf78ebfe7a065de35ff453077a7d
Jackelin <jackelinvasquezguzman@gmail.com>
2025-08-07 16:09:09 -0600

    actividad

--- Actividad 16.py
+++ Actividad 16.py
@@ -8,13 +8,29 @@ class Libro:
         self.year=year
 
     def mostrar_informacion(self):
-
+        print(f"---Información de libro---")
+        print(f"Titulo:{self.titulo}\nAutor:{self.autor}\nAño:{self.year}")
 
 def agregar_libro():
     libro_agregar= input("Ingrese titulo de libro:")
-    autor= input("Ingrese autor de libro:")
-    year= int(input("Ingrese año de publicación:"))
-    libros.append(Libro)
+    if libro_agregar in libros:
+        print("Libro ya registrado...")
+    else:
+        autor= input("Ingrese autor de libro:")
+        year= int(input("Ingrese año de publicación:"))
+        libro= Libro(libro_agregar,autor,year)
+        libros.append(libro)
+def mostrar_libros():
+    print("---LIBROS INGRESADOS---")
+    for i in libros:
+        print(f"---Información libro {i+1}--")
+        Libro.mostrar_informacion()
+def eliminar_libro():
+    libro_eliminar= input("Ingrese nombre de libro a eliminar:")
+    if libro_eliminar in libros:
+        libros.remove(libro_eliminar)
+
+
 
 def menu_principal():
     print(f"---MENÚ PRINCIPAL---\n1.agregar libro.\n2.Mostrar la lista de libros.")
